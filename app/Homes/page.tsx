a399ca5638059a57a8aafdbb67bd4b6f1ed73c4d
afreenme-22 <arjumandafreen407@gmail.com>
2024-12-19 21:24:35 +0500

    Updated page.tsx

--- app/Homes/page.tsx
+++ app/Homes/page.tsx
@@ -12,15 +12,16 @@ const Homes = () => {
       className="homes"
     >
       <div className="homes-text">
-        
         <h1 style={{ fontSize: "3.5rem" }}>Feel the Pearl Difference</h1>
-
         <p>
-          More than 50 years of hospitality experience brings you the best hotels in Pakistan, where luxury meets tradition, and every stay is a journey of refined elegance and unparalleled comfort.{" "}
+          More than 50 years of hospitality experience brings you the best hotels in Pakistan, 
+          where luxury meets tradition, and every stay is a journey of refined elegance and unparalleled comfort. 
           <br />
-          Overlooking the sprawling city, Hotel Pearl Karachi is a part of the most significant chain of hotels in Pakistan. The Hotel is perfect for both business and leisure travelers. Guests can choose from a variety of rooms and suites to enjoy their stay at the five-star hotel.
+          Overlooking the sprawling city, Hotel Pearl Karachi is a part of the most significant chain of hotels in Pakistan. 
+          The Hotel is perfect for both business and leisure travelers. Guests can choose from a variety of rooms and suites 
+          to enjoy their stay at the five-star hotel.
         </p>
-        <Link href={"#"}>
+        <Link href="#">
           <button className="btn">Booking Continues</button>
         </Link>
       </div>
